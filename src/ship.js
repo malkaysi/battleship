b5ab45e30b9ca3c5e477ba01ca0fa7b989d055db
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2022-07-29 23:54:32 -0400

    thinking through the solution

---
+++ src/ship.js
@@ -0,0 +1,9 @@
+// Ship factory function
+// Object that should include length, where they've been hit, and whether they've sunk
+
+// They are placed on a gameboard that has coordinates
+// Coordinates are assigned to the ship based on its length
+
+// If a coordinate matches the ship, it's a hit
+// Once all the coordinates are hit, it's sunk
+// Coordinates cannot be clicked twice
